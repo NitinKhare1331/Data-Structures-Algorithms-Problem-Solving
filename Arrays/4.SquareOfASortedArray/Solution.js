727846ae32ae4a7b04cc0b7c8fde7bfb5344a3e0
Nitin Khare <kharenitin777@gmail.com>
2024-02-22 12:31:49 +0530

    code added

---
+++ Arrays/4.SquareOfASortedArray/Solution.js
@@ -0,0 +1,24 @@
+/*
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+*/
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+var SortedSquares = function (nums) {
+    return nums.map((num)=>num*num).sort((a,b)=>a-b);
+}
+
+let nums = [-7,-3,2,3,11];
+let result = SortedSquares(nums);
+console.log(result);
