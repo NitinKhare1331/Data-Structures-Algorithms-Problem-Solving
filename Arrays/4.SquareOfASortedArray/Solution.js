61a0e8c0bec0896379fd626397f612a04f96fe8e
Nitin Khare <kharenitin777@gmail.com>
2024-03-06 15:13:31 +0530

    code added

--- Arrays/4.SquareOfASortedArray/Solution.js
+++ Arrays/4.SquareOfASortedArray/Solution.js
@@ -15,8 +15,36 @@ Output: [4,9,9,49,121]
 
 //https://leetcode.com/problems/squares-of-a-sorted-array/description/
 
-var SortedSquares = function (nums) {
-    return nums.map((num)=>num*num).sort((a,b)=>a-b);
+// var SortedSquares = function (nums) {
+//     return nums.map((num)=>num*num).sort((a,b)=>a-b);
+// }
+
+// function sqrSrt(nums){
+//     let n = nums.length-1;
+//     for (let i=0 ;i<nums.length; i++){
+//         nums[i] = nums[i]**2;
+//     }
+//     console.log(nums.sort((a,b)=>a-b));
+// }
+
+// console.log(sqrSrt([1,2,3,4,5,6]));
+
+function SortedSquares(nums){
+    let n = nums.length;
+    let result = Array(n);
+    let left  = 0;
+    let right = n-1;
+    for (let i = n-1; i >= 0; i--){
+        if(nums[left]**2 < nums[right]**2){
+            result[i] = nums[right]**2;
+            right--;
+        }
+        else {
+            result[i] = nums[left]**2;
+            left++;
+        }
+    }
+    return result
 }
 
 let nums = [-7,-3,2,3,11];
---
+++ Arrays/6.3Sum/solution.js
@@ -0,0 +1,66 @@
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+*/
+
+var threeSum = function (nums){
+    nums.sort((a,b)=> a-b);
+    let n = nums.length;
+    let resultArr = [];
+    for(let i=0; i<n; i++){
+        let a = i+1;
+        let b = n-1;
+        while(a<b){
+            let sum = nums[i] + nums[a] + nums[b];
+            if(sum == 0){
+                resultArr.push([nums[i],nums[a],nums[b]]);
+                while(a<b && nums[a]==nums[a+1]){
+                    a++;
+                }
+                a++;
+                while(a>b && nums[b]==nums[b-1]){
+                    b--;
+                }
+                b--;
+            }
+            else if(sum<0){
+                a++;
+            }
+            else{
+                b--;
+            }
+        }
+        while(i<n-1 && nums[i]==nums[i+1]){
+            i++
+        }
+    }
+    return resultArr
+}
+
+let nums = [-1,0,1,2,-1,-4];
+let result = threeSum(nums);
+console.log(result);
